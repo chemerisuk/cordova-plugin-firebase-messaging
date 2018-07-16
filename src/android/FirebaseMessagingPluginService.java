b8b1b1ce4b53b15a05c30ce7629c37f038db01a1
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-07-17 01:17:04 +0300

    drop deprecated FirebaseInstanceIDService

--- src/android/FirebaseMessagingPluginService.java
+++ src/android/FirebaseMessagingPluginService.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 import android.content.Intent;
 import android.util.Log;
-import android.support.v4.content.LocalBroadcastManager;
+// import android.support.v4.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,8 +15,8 @@ import org.json.JSONObject;
 
 public class FirebaseMessagingPluginService extends FirebaseMessagingService {
     private static final String TAG = "FirebaseMessagingPlugin";
-    public static final String ACTION_FCM_DATA = "by.chemerisuk.cordova.firebase.FCM_DATA_EVENT";
-    public static final String KEY_FCM_DATA = "RemoteMessageData";
+    // public static final String ACTION_FCM_DATA = "by.chemerisuk.cordova.firebase.FCM_DATA_EVENT";
+    // public static final String KEY_FCM_DATA = "RemoteMessageData";
 
     private LocalBroadcastManager broadcastManager;
 
@@ -26,16 +26,23 @@ public class FirebaseMessagingPluginService extends FirebaseMessagingService {
     }
 
     @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        FirebaseMessagingPlugin.sendInstanceId(token);
+    }
+
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         try {
             JSONObject json = createNotificationJSON(remoteMessage);
             FirebaseMessagingPlugin.sendNotification(json, false);
 
-            if (!remoteMessage.getData().isEmpty()) {
-                Intent intent = new Intent(ACTION_FCM_DATA);
-                intent.putExtra(KEY_FCM_DATA, new HashMap(remoteMessage.getData()));
-                this.broadcastManager.sendBroadcast(intent);
-            }
+            // if (!remoteMessage.getData().isEmpty()) {
+            //     Intent intent = new Intent(ACTION_FCM_DATA);
+            //     intent.putExtra(KEY_FCM_DATA, new HashMap(remoteMessage.getData()));
+            //     this.broadcastManager.sendBroadcast(intent);
+            // }
         } catch (JSONException e) {
             Log.e(TAG, "onMessageReceived", e);
         }
