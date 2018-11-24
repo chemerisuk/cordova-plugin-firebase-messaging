48446a8eb6c3364fada4b11e66de7e1677c1f63f
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-11-24 10:44:54 +0300

    broadcast local intents from FirebaseMessagingPluginService

--- src/android/FirebaseMessagingPluginService.java
+++ src/android/FirebaseMessagingPluginService.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 import android.content.Intent;
 import android.util.Log;
-// import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,29 +12,34 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessagingPluginService extends FirebaseMessagingService {
     private static final String TAG = "FirebaseMessagingPlugin";
-    // public static final String ACTION_FCM_DATA = "by.chemerisuk.cordova.firebase.FCM_DATA_EVENT";
-    // public static final String KEY_FCM_DATA = "RemoteMessageData";
 
-    // private LocalBroadcastManager broadcastManager;
+    public static final String ACTION_FCM_MESSAGE = "by.chemerisuk.cordova.firebase.ACTION_FCM_MESSAGE";
+    public static final String EXTRA_FCM_MESSAGE = "by.chemerisuk.cordova.firebase.EXTRA_FCM_MESSAGE";
+    public static final String ACTION_FCM_TOKEN = "by.chemerisuk.cordova.firebase.ACTION_FCM_TOKEN";
+    public static final String EXTRA_FCM_TOKEN = "by.chemerisuk.cordova.firebase.EXTRA_FCM_TOKEN";
 
-    // @Override
-    // public void onCreate() {
-    //     this.broadcastManager = LocalBroadcastManager.getInstance(this);
-    // }
+    private LocalBroadcastManager broadcastManager;
+
+    @Override
+    public void onCreate() {
+        this.broadcastManager = LocalBroadcastManager.getInstance(this);
+    }
 
     @Override
     public void onNewToken(String token) {
         FirebaseMessagingPlugin.sendInstanceId(token);
+
+        Intent intent = new Intent(ACTION_FCM_TOKEN);
+        intent.putExtra(EXTRA_FCM_TOKEN, token);
+        this.broadcastManager.sendBroadcast(intent);
     }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         FirebaseMessagingPlugin.sendNotification(remoteMessage);
 
-        // if (!remoteMessage.getData().isEmpty()) {
-        //     Intent intent = new Intent(ACTION_FCM_DATA);
-        //     intent.putExtra(KEY_FCM_DATA, new HashMap(remoteMessage.getData()));
-        //     this.broadcastManager.sendBroadcast(intent);
-        // }
+        Intent intent = new Intent(ACTION_FCM_MESSAGE);
+        intent.putExtra(EXTRA_FCM_MESSAGE, remoteMessage);
+        this.broadcastManager.sendBroadcast(intent);
     }
 }
