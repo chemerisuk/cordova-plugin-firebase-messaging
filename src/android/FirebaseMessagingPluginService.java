8633f840dd77e8e37dae1d58232d33b14ecb6f3b
Maksim Chemerisuk <chemerisuk@gmail.com>
2019-04-24 11:50:07 +0300

    fixes #67: move to androidx

--- src/android/FirebaseMessagingPluginService.java
+++ src/android/FirebaseMessagingPluginService.java
@@ -1,7 +1,7 @@
 package by.chemerisuk.cordova.firebase;
 
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
