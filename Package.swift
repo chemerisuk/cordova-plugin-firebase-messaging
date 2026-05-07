29eb121d7d87ecd2dfe63b1dfb4026fc1612f381
Maksim Chemerisiuk <chemerisuk@gmail.com>
2026-05-07 11:47:50 +0300

    fixing Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,27 @@
 // swift-tools-version:5.9
+
 import PackageDescription
 
 let package = Package(
-    name: "cordova-plugin-firebase-messaging",
-    platforms: [
-        .iOS(.v13)
-    ],
+    name: "cordova-plugin-echo",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "cordova-plugin-firebase-messaging", targets: ["cordova-plugin-firebase-messaging"])
     ],
     dependencies: [
-        .package(url: "https://github.com", from: "11.0.0")
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.13.0")
     ],
     targets: [
         .target(
             name: "cordova-plugin-firebase-messaging",
             dependencies: [
+                .product(name: "Cordova", package: "cordova-ios"),
                 .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
             ],
-            path: "src/ios"
+            path: "src/ios",
+            resources: [],
+            publicHeadersPath: "."
         )
     ]
 )
