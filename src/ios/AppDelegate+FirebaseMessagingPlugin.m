a751a1d0ee1408a8b92df34fd83b858eae9ca99a
Maksim Chemerisiuk <chemerisuk@gmail.com>
2026-07-09 12:55:44 +0300

    Update implementation of didReceiveRemoteNotification

--- src/ios/AppDelegate+FirebaseMessagingPlugin.m
+++ src/ios/AppDelegate+FirebaseMessagingPlugin.m
@@ -55,6 +55,17 @@
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    // 1. Thread-safe block wrapper to prevent duplicate completionHandler execution.
+    // If Firebase SDK auto-proxy and this plugin both call it, the app won't crash.
+    __block BOOL completionHandlerCalled = NO;
+    void (^safeCompletionHandler)(UIBackgroundFetchResult) = ^(UIBackgroundFetchResult result) {
+        if (!completionHandlerCalled) {
+            completionHandlerCalled = YES;
+            completionHandler(result);
+        }
+    };
+
+    // 2. Process notification payload within the plugin
     FirebaseMessagingPlugin* fcmPlugin = [self getPluginInstance];
     if (application.applicationState != UIApplicationStateActive) {
         [fcmPlugin sendBackgroundNotification:userInfo];
@@ -62,7 +73,23 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
         [fcmPlugin sendNotification:userInfo];
     }
 
-    completionHandler(UIBackgroundFetchResultNewData);
+    // 3. Clean approach to forward the call to the original implementation (super)
+    // This dynamically fetches the original IMP to bypass performSelector limitations with 3 arguments.
+    SEL originalSelector = @selector(application:didReceiveRemoteNotification:fetchCompletionHandler:);
+
+    if ([super respondsToSelector:originalSelector]) {
+        // Get the implementation of the super class method directly
+        IMP superMethodImp = [super methodForSelector:originalSelector];
+
+        // Cast the implementation function pointer to match the method signature
+        void (*fwdCall)(id, SEL, UIApplication *, NSDictionary *, void (^)(UIBackgroundFetchResult)) = (void *)superMethodImp;
+
+        // Execute the original method implementation seamlessly
+        fwdCall(self, originalSelector, application, userInfo, safeCompletionHandler);
+    } else {
+        // Fallback fallback if no other plugin or main AppDelegate implements this method
+        safeCompletionHandler(UIBackgroundFetchResultNewData);
+    }
 }
 
 # pragma mark - UNUserNotificationCenterDelegate
